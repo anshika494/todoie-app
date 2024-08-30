3f9b3d375b28cca940930681c60e5051f1811e59
anshika494 <anshi040904@gmail.com>
2024-08-31 03:28:29 +0530

    added static todoie using react

---
+++ src/components/toDoItem.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ToDoItem = () => {
+    return (
+        <li className="todo-item">
+            <span>
+            <input type="checkbox" />
+            <span className="todo-item-text">Eat</span>
+            </span>
+            <p>...</p>
+            </li>
+    );
+}
+
+export default ToDoItem;
